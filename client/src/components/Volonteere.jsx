7d119b3ad9b491b373da894aaac1354181f1edc2
tehila_m <tehila_m@malam.com>
2022-05-25 00:43:22 +0300

    gallery and beginning of volunteering

--- client/src/components/Volonteere.jsx
+++ client/src/components/Volonteere.jsx
@@ -1,10 +1,38 @@
 import "../styles/homePage.css"
+import { useFormControl } from '@mui/material/FormControl';
+import TextField from '@material-ui/core/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 export const Volonteere = () => {
 
     return (
         <div>
-        התנדבות 
+        <h1>???מעוניינים להתנדב</h1> 
+        <h2>נא מלאו את טופס ההצטרפות ונחזור אליכם בהקדם</h2>
+        <div>שם פרטי ושם משפחה</div>
+        <TextField id="outlined-basic" label="" variant="outlined" />
+        <div>טלפון לייצירת קשר</div>
+        <TextField
+          id="outlined-number"
+          label=""
+          variant="outlined"
+          inputProps={{ inputMode: 'tel', pattern: '[0-9]' }} />
+        <div></div>
+        <FormControlLabel control={<Checkbox defaultChecked />} label="מקבל SMS" />
+        <div>דוא"ל</div>
+        <TextField id="outlined-basic" label="" variant="outlined" />
+        <div>הערות</div>
+        <TextField
+    dir="rtl"
+          id="outlined-multiline-static"
+          label=""
+          multiline
+          rows={4}
+          variant="outlined"
+          defaultValue="הייתי שמח להתנדב במחלקת..."
+        />
         </div>
     )
 }
