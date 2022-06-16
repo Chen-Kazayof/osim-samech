b7173ce155a70eb1d496ba9094b7e20e5a0bd4d7
yaeloria389@gmail.com <yaeloria389@gmail.com>
2022-06-17 02:54:57 +0300

    fin all

--- client/src/components/FunWagonChild.jsx
+++ client/src/components/FunWagonChild.jsx
@@ -1,45 +1,72 @@
-import "../styles/FunWagonChild.css"
+import "../styles/Mesamchim_act.css"
 
 export const FunWagonChild = () => {
 
     return (
-        <div>
-            <div className="subtitle_fun_child">עגלת פינוקים לילדים</div>
-            <div className="text_fun_child">,עגלת הילדים מלאה בכיף, פינוקים וכל מה שמשמח ילדים: פופקורן טעים, קרפ מתוק, אייס קפה מרענן, וופל בלגי חם</div>
-            <div className="text_fun_child">.ימבמבם ועוד ים של ממתקים ושוקולדים</div>
-            <div className="text_fun_child">.ההיגיון המתוק אומר שגם לילד מגיע משהו קטן שיחזק אותו בשעות של התמודדות</div>
-            <div className="text_fun_child">...זו יכולה להיות מילה טובה שמגיעה בדיוק בזמן ועוזרת לעבור עוד יום ארוך או עוד חיוך שיעלה את מצב הרוח אחרי יום גשם</div>
-            <div className="text_fun_child">,העגלה מטעינה את הילדים בכוח ובשמחה עם שיחות גדולות על הרגעים הקטנים והמאתגרים בין ביס לביסלי ובין לק לסוכריה ועוד נשנושים</div>
-            <div className="text_fun_child">...כי זאת המורכבות של החיים</div>
-            <div className="text_fun_child">!ובסופו של דבר חיוך הוא הדבר הכי מתוק בעולם</div>
 
-            <table className="img_table_child">
-                <tr className="img_row">
-                    <th><img className="imgchild1" src="child/1.png" alt="not" /></th>
-                    <td><img className="imgchild" src="child/2.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/3.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/4.png" alt="not" /></td>
-                </tr>
-                <tr className="img_row">
-                    <td><img className="imgchild1" src="child/5.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/6.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/7.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/8.png" alt="not" /></td>
-                </tr>
-                <tr className="img_row">
-                    <td><img className="imgchild1" src="child/9.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/10.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/11.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/12.png" alt="not" /></td>
-                </tr>
-                <tr className="img_row">
-                    <td><img className="imgchild1" src="child/13.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/14.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/15.png" alt="not" /></td>
-                    <td><img className="imgchild" src="child/16.png" alt="not" /></td>
-                </tr>
-            </table>
+        <div>
+
+
+            <div className="m_div_size">
+
+                <ul className-="tabel-mesameach m_ul">
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c1.jpg" alt="Landscape" />
+                    </li>
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c2.jpg" alt="Landscape" />
+                    </li >
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c6.jpg" alt="Landscape" />
+                    </li>
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c4.jpg" alt="Landscape" />
+                    </li>
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c5.jpg" alt="Landscape" />
+                    </li>
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c3.jpg" alt="Landscape" />
+                    </li>
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c7.jpg" alt="Landscape" />
+                    </li>
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c8.jpg" alt="Landscape" />
+                    </li>
+
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c9.jpg" alt="Landscape" />
+                    </li>
+                    <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c10.jpg" alt="Landscape" />
+                    </li>
+                    {/* <li className-="line-mesameach m_li">
+                        <img className="m_img_meshamchim" src="c11.jpg" alt="Landscape" />
+                    </li> */}
+
+                </ul>
+
+            </div>
+            <div className="bottom-right-mesameach">
+                <div className="koteret_mesamchim">עגלת ילדים</div>
+                <div>
+                    <div className="class_mesamach">עגלת הילדים מלאה בכיף, פינוקים וכל מה שמשמח ילדים: פופקורן טעים, קרפ מתוק, אייס קפה
+                        מרענן, וופל בלגי חם, ימבמבם ועוד ים של ממתקים ושוקולדים.
+                        ההגיון המתוק אומר שגם לילד מגיע משהו קטן שיחזק אותו בשעות של התמודדות. זו יכולה להיות
+                        מילה טובה שמגיעה בדיוק בזמן ועוזרת לעבור עוד יום ארוך או עוד חיוך שיעלה את מצב הרוח אחרי
+                        יום של גשם ...
+                        העגלה מטעינה את הילדים בכח ובשמחה עם שיחות גדולות על הרגעים הקטנים והמאתגרים בין
+                        ביס לביסלי לבין לק לסוכריה ועוד נשנושים, כי זאת המורכבות של החיים ...
+                        ובסופו של דבר חיוך הוא הדבר הכי מתוק בעולם!
+                    </div>
+                </div>
+
+                <div className="y_under_Line_mesamchim"></div>
+            </div>
 
         </div>
-    );
+
+    )
+
 }
